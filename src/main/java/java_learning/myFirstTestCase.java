3cdc928d46583ea80532e995c49eefedb5437554
Zakir <79053961+zakirizu@users.noreply.github.com>
2025-04-16 18:29:11 +0530

    Master
    Master

--- src/main/java/java_learning/myFirstTestCase.java
+++ src/main/java/java_learning/myFirstTestCase.java
@@ -5,6 +5,7 @@ public class myFirstTestCase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bismillah");
+		System.out.println("TEST FROM MASTER");
 		System.out.println("Zakir");
 		System.out.println("Aman");
 		System.out.println("Zakriya");
